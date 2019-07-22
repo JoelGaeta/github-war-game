42c2191044d852e39d84ec2bc72e58178e05b7aa
Joel Gaeta <joelsgaeta@gmail.com>
2019-07-22 16:16:15 -0400

    production configuation

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
     ]
   },
 
-  mode: "development",
+  mode: process.env.NODE_ENV === "production" ? "production" : "development",
   plugins: [
     new HtmlWebPackPlugIn({
       template: "app/index.html"
