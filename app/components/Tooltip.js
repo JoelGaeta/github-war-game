bb0a98a0adcee5082262f0a1b3491941f5b98ee3
Joel Gaeta <joelsgaeta@gmail.com>
2019-07-22 20:40:38 -0400

    hover changed to Hover when being imported to tooltip

--- app/components/Tooltip.js
+++ app/components/Tooltip.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import Hover from "./hover";
+import Hover from "./Hover";
 const styles = {
   container: {
     position: "relative",
